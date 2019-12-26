189e55221f7556855bde7297a22c152bb5b08777
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-12-26 12:23:13 -0700

    create account page created

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -11,6 +11,22 @@ $("#login-button").on("click", function() {
     var email = $("#email-input").val().trim();
     var password = $("#password-input").val().trim();
 
+    console.log("Loging in....")
+    console.log("N: "+name,"E: "+email,"P: "+password)
+})
+
+
+
+
+// ** Create Account Page **
+
+$("#ca-acct-button").on("click", function() {
+
+    var name = $("#ca-name-input").val().trim();
+    var email = $("#ca-email-input").val().trim();
+    var password = $("#ca-password-input").val().trim();
+    
+    console.log("Account created....")
     console.log("N: "+name,"E: "+email,"P: "+password)
 })
 
