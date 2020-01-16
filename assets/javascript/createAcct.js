240d580f3b4d4391fea223d16f9a3bf7ae5bd031
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2020-01-16 14:11:00 -0700

    setting up server

---
+++ assets/javascript/createAcct.js
@@ -0,0 +1,28 @@
+var mysql = require("mysql");
+
+console.log("CA.JS || SQL running..")
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "",
+    database: "sportsCorner",
+});
+
+connection.connect(function (err) {
+    console.log("connected as id " + connection.threadId)
+    afterConnection()
+});
+
+function afterConnection() {
+    console.log("After Connection Fired off")
+}
+
+
+
+connection.end()
---
+++ server.js
@@ -0,0 +1,36 @@
+
+// Require/import the HTTP module
+var express = require("express");
+var http = require("http");
+var mysql = require("mysql");
+var fs = require("fs");
+
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get("/", function(req, res) {
+    // res.send("Welcome to the Star Wars Page!")
+  
+    // change the file name to switch that to my new html file.
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
